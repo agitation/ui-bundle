05afc6f3d5ce34063a6848a7746ff04ff28c5f68
Alex Günsche <mail@lxg.de>
2015-06-15 16:19:00 +0200

    auto-switch to user-preferred language if alternative page exists

---
+++ Resources/public/js/lib/langswitch.js
@@ -0,0 +1,57 @@
+/*jslint bitwise: false, continue: false, debug: false, eqeq: true, es5: false, evil: false, forin: false, newcap: false, nomen: true, plusplus: true, regexp: true, undef: false, unparam: true, sloppy: true, stupid: false, sub: false, todo: true, vars: false, white: true, css: false, on: false, fragment: false, passfail: false, browser: true, devel: true, node: false, rhino: false, windows: false, indent: 4, maxerr: 100 */
+
+/*
+  NOTE:
+    - executes immediately
+    - stops the loading of the page
+    - Must be inserted AFTER the hreflang links
+    - Depends on cookies.js
+    - Does not depend on jQuery
+*/
+
+(function(){
+    var
+        cookieName = 'agit.ui.lang.auto',
+        autoLang = Cookies.get(cookieName),
+
+        run = function()
+        {
+            var
+                browserLocale = navigator.language.toLowerCase().replace('-', '_'),
+                browserLang = browserLocale.substr(0, 2),
+
+                availableLanguages = (function(){
+                    var
+                        langs = {},
+                        $links = document.querySelectorAll('head link[rel=alternate][hreflang][href]'),
+                        i = 0;
+
+                    for (i; i<$links.length; i++)
+                    {
+                        langs[$links[i].getAttribute('hreflang').toLowerCase()] = $links[i].getAttribute('href');
+                    }
+
+                    return langs;
+                })(),
+
+                switchToLang = function(lang, url)
+                {
+                    Cookies.set(cookieName, lang, {path: '/'});
+                    window.location.href = url;
+                };
+
+            if (availableLanguages[browserLocale])
+            {
+                switchToLang(browserLocale, availableLanguages[browserLocale]);
+            }
+            else if (availableLanguages[browserLang])
+            {
+                switchToLang(browserLang, availableLanguages[browserLang]);
+            }
+        };
+
+    // We will only switch once. If somebody really wants to see pages
+    // in different languages than his preferred one, we won't stop him.
+
+    if (!autoLang) { run(); }
+})();
