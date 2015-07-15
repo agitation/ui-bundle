561dbccce72d87bab1d057bb86db677038ca6b91
Alex Günsche <mail@lxg.de>
2015-07-15 23:43:46 +0200

    make overlay a bit less obtrusive

--- Resources/public/js/lib/Overlay.js
+++ Resources/public/js/lib/Overlay.js
@@ -16,7 +16,7 @@ Agit.Overlay = new (function()
 
         if (count === 1)
         {
-            $overlay.css('opacity', 0.5).show();
+            $overlay.css('opacity', 0.4).show();
         }
     };
 
