e6952c23ca4c537df4e865b62084b0d78a187f75
Alex Günsche <mail@lxg.de>
2015-11-15 23:26:30 +0100

    pages plugin now collects pages from all bundles

--- Plugin/UiPagePlugin.php
+++ Plugin/UiPagePlugin.php
@@ -9,15 +9,173 @@
 
 namespace Agit\UiBundle\Plugin;
 
+use Agit\CommonBundle\Exception\InternalErrorException;
+use Agit\PluggableBundle\Strategy\ServiceAwarePluginTrait;
+use Agit\PluggableBundle\Strategy\ServiceAwarePluginInterface;
+use Agit\UiBundle\TwigMeta\PageConfigNode;
+use Agit\PluggableBundle\Strategy\Depends;
+use Agit\PluggableBundle\Strategy\Cache\CacheEntry;
 use Agit\PluggableBundle\Strategy\Cache\CachePlugin;
+use Agit\PluggableBundle\Strategy\Cache\CachePluginInterface;
+
 
 /**
  * @CachePlugin(tag="agit.ui.pages")
+ * @Depends({"kernel", "agit.common.filecollector", "twig"})
+ *
+ * This plugin walks through *all* bundles and searches the Resources/views directory
+ * for compatible pages.
+ *
+ * NOTE: Bundles can override other bundles's pages. The order is determined by the
+ * bundle loading order in AppKernel.
  */
-class UiPagePlugin extends AbstractUiPagePlugin
+final class UiPagePlugin implements CachePluginInterface, ServiceAwarePluginInterface
 {
-    protected function getSearchPath()
+    use ServiceAwarePluginTrait;
+
+    private $availableTypes = ['page' => 'Page', 'special' => 'Special'];
+
+    private $entryList = [];
+
+    public function load()
     {
-        return "AgitUiBundle:Resources:views";
+        $fileCollector = $this->getService('agit.common.filecollector');
+
+        foreach ($this->getService("kernel")->getBundles() as $alias => $bundle)
+        {
+            foreach ($this->availableTypes as $type => $subdir)
+            {
+                $extension = "html.twig";
+                $basePath = $fileCollector->resolve("$alias:Resources:views:$subdir");
+
+                foreach ($fileCollector->collect($basePath, $extension) as $pagePath)
+                {
+                    $cacheEntry = new CacheEntry();
+                    $data = $this->getData($type, $subdir, $basePath, $pagePath, $extension);
+
+                    $cacheEntry->setId($data['vPath']);
+                    $cacheEntry->setData($data);
+                    $this->entryList[] = $cacheEntry;
+                }
+            }
+        }
+    }
+
+    public function nextCacheEntry()
+    {
+        return array_shift($this->entryList);
+    }
+
+    protected function getData($type, $subdir, $basePath, $pagePath, $extension)
+    {
+        $page = str_replace(["$basePath/", ".$extension"], '', $pagePath);
+
+        $data = [
+            'type'      => $type,
+            'vPath'     => ($type === 'page') ? $this->pageToVirtualPath($page) : '_' . basename($page),
+            'template'  => $this->pathToTemplateName($basePath, $page, $extension),
+            'order'     => $this->getOrderPosition($page)
+        ];
+
+        $config = $this->getConfigFromTemplate($pagePath);
+
+        if (!isset($config['capability']))
+            throw new InternalErrorException("Template {$data['template']} does not define capabilities.");
+
+        $data['caps'] = (string)$config['capability'];
+
+        $data['pageId'] = $this->makePageId($data['vPath']); // NOTE: The page ID is unique only within its page set.
+        $data['status'] = isset($config['status']) ? (int)$config['status'] : 200;
+
+        $twigTemplate = $this->getService('twig')->loadTemplate($data['template']);
+        $hasParent = (bool)$twigTemplate->getParent([]);
+        $data['isVirtual'] = !$hasParent; // a rather simple convention, but should be ok for our scenarios
+        $data['name'] = $twigTemplate->renderBlock('title', []);
+
+        if ($data['isVirtual'])
+            unset($data['template'], $data['pageId']);
+
+        return $data;
+    }
+
+    protected function pageToVirtualPath($page)
+    {
+        $parts = preg_split('|/+|', $page, null, PREG_SPLIT_NO_EMPTY);
+
+        $parts = array_map(function($part) {
+            // if the first part is numeric, it is for ordering and must be chopped off
+            return preg_replace('|^\d{1,3}\.|', '', $part);
+        }, $parts);
+
+        $parts = array_filter($parts, function($part) {
+            return ($part !== 'index' && $part !== '');
+        });
+
+        return '/' . implode('/', $parts);
+    }
+
+    protected function pathToTemplateName($basePath, $page, $extension)
+    {
+        return "$basePath/$page.$extension";
+    }
+
+    protected function getOrderPosition($page)
+    {
+        $pos = null;
+        $parts = preg_split('|/+|', $page, null, PREG_SPLIT_NO_EMPTY);
+
+        if (count($parts))
+        {
+            $last = array_pop($parts);
+
+            // when it's an index page, then the order must be determined via the parent directory.
+            if ($last === 'index' && count($parts))
+                $last = array_pop($parts);
+
+            if (preg_match('|^(\d{1,3})\.|', $last, $matches) && is_array($matches) && isset($matches[1]))
+                $pos = (int)$matches[1];
+        }
+
+        return $pos;
+    }
+
+    private function makePageId($vPath)
+    {
+        $pageFilename = '';
+        $pathParts = explode('/', trim($vPath, '/_'));
+        $pageFilename .= array_shift($pathParts);
+        $pathParts = array_map('ucfirst', $pathParts);
+        $pageFilename .= implode('', $pathParts);
+
+        if ($pageFilename === '')
+            $pageFilename = 'index';
+
+        return $pageFilename;
+    }
+
+    private function getConfigFromTemplate($pagePath)
+    {
+        $tokenStream = $this->getService('twig')->tokenize(file_get_contents($pagePath));
+        $rootNode = $this->getService('twig')->parse($tokenStream);
+
+        return $this->findConfigInNode($rootNode);
+    }
+
+    private function findConfigInNode($node)
+    {
+        $config = [];
+
+        foreach ($node->getIterator() as $childNode)
+        {
+            if ($childNode instanceof \Twig_Node)
+            {
+                if ($childNode instanceof PageConfigNode)
+                    $config += $childNode->getConfigValues();
+
+                $config += $this->findConfigInNode($childNode);
+            }
+        }
+
+        return $config;
     }
 }
