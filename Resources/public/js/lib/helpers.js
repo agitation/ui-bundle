6a9e636b1828cf97836c1145c49b3951d40d05bd
Alex Günsche <mail@lxg.de>
2015-11-28 23:34:24 +0100

    remove trim function; use the one from jQuery instead

--- Resources/public/js/lib/helpers.js
+++ Resources/public/js/lib/helpers.js
@@ -1,23 +1,6 @@
 /*jslint bitwise: false, continue: false, debug: false, eqeq: true, es5: false, evil: false, forin: false, newcap: false, nomen: true, plusplus: true, regexp: true, undef: false, unparam: true, sloppy: true, stupid: false, sub: false, todo: true, vars: false, white: true, css: false, on: false, fragment: false, passfail: false, browser: true, devel: true, node: false, rhino: false, windows: false, indent: 4, maxerr: 100 */
 /*global Agit, $, jQuery */
 
-Agit.trim = function(string)
-{
-    if (string === undefined || string === null)
-    {
-        string = '';
-    }
-    else if (string.replace === undefined)
-    {
-        string = string.toString();
-    }
-
-    string = string.replace(/^\s+/, '');
-    string = string.replace(/\s+$/, '');
-
-    return string;
-};
-
 Agit.ucfirst = function(string)
 {
   return string.charAt(0).toUpperCase() + string.substr(1);
