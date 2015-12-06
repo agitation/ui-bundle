4b2c89096831bc337ead3c9427645cc6cf66ae99
Alex Günsche <mail@lxg.de>
2015-12-06 22:34:07 +0100

    complete refactoring as IIFE

--- Resources/public/js/lib/ApiCall.js
+++ Resources/public/js/lib/ApiCall.js
@@ -1,44 +1,37 @@
 /*jslint bitwise: false, continue: false, debug: false, eqeq: true, es5: false, evil: false, forin: false, newcap: false, nomen: true, plusplus: true, regexp: true, undef: false, unparam: true, sloppy: true, stupid: false, sub: false, todo: true, vars: false, white: true, css: false, on: false, fragment: false, passfail: false, browser: true, devel: true, node: false, rhino: false, windows: false, indent: 4, maxerr: 100 */
 /*global Tx, $, jQuery, OpenLayers, JSON */
 
-Agit.ApiCall = function(endpoint, requestObject, successCallback, _params)
-{
+Agit.apiCall = (function(){
     var
-        defaultParams = {
-            indicator      : Agit.Service.get('indicator'),
-            messageHandler : Agit.Service.get('messageHandler'),
-            dataType       : 'json',
-            callback       : function(){},
-            processType    : 'api'  // 'api', 'apicomplete', or 'raw'. with 'api*', we expect an API response and will preprocess it
-        },
+        // to be set by the API-call function
+        messageHandler,
 
-        params = $.extend(defaultParams, _params || {}),
-
-        processPayload = function(payload, entityList)
+        normalizePayload = function(payload, entityList)
         {
             var
                 pattern = /#e#:[0-9]+/,
+
                 expand = function(value)
                 {
-                    var
-                        type = $.type(value),
-                        newValue = value;
+                    var newValue = value;
 
-                    if (type === 'object')
-                    {
-                        newValue = {};
-                        $.each(value, function(k, v){
-                            newValue[k] =  expand(v);
-                        });
-                    }
-                    else if (type === 'array')
+                    if (value instanceof Array)
                     {
                         newValue = [];
+
                         $.each(value, function(k, v){
                             newValue.push(expand(v));
                         });
                     }
-                    else if (type === 'string' && value.match(pattern))
+                    else if (value instanceof Object)
+                    {
+                        newValue = {};
+
+                        $.each(value, function(k, v){
+                            newValue[k] =  expand(v);
+                        });
+                    }
+                    else if (typeof(value) === "string" && value.match(pattern))
                     {
                         newValue = expand(entityList[value]);
                     }
@@ -49,34 +42,31 @@ Agit.ApiCall = function(endpoint, requestObject, successCallback, _params)
             return expand(payload);
         },
 
-        processMessages = function(messageList)
+        showMessages = function(messages)
         {
-            var i = 0;
-
-            if (messageList.length > 0)
+            if (messageHandler)
             {
-                params.messageHandler.clear('agit.api');
+                messageHandler.clear("agit.api");
 
-                for (i; i < messageList.length; i++)
-                {
-                    params.messageHandler.showMessage(new Agit.Message(
-                        messageList[i].text,
-                        messageList[i].type,
-                        'agit.api'
+                messages.forEach(function(message){
+                    messageHandler.showMessage(new Agit.Message(
+                        message.text,
+                        message.type,
+                        "agit.api"
                     ));
-                }
+                });
             }
         },
-        combinedCallback = function(res)
-        {
-            var finishCallback = function(){};
 
-            if (!res ||
-                typeof(res) !== 'object' ||
-                res.payload === undefined ||
-                res.messageList === undefined)
+        // this is to make sure that, in the event of an error, we have a "proper" response.
+        processResponse = function(response)
+        {
+            if (!response ||
+                typeof(response) !== "object" ||
+                response.payload === undefined ||
+                response.messageList === undefined)
             {
-                res =
+                response =
                 {
                     success : false,
                     payload : null,
@@ -84,86 +74,54 @@ Agit.ApiCall = function(endpoint, requestObject, successCallback, _params)
                 };
             }
 
-            if (['api', 'apicomplete'].indexOf(params.processType) > -1)
-            {
-                finishCallback = function()
-                {
-                    processMessages(res.messageList);
-                    res.payload = processPayload(res.payload, res.entityList);
-                    successCallback(params.processType === 'api' ? res.payload : res);
-                }
-            }
-            else if (params.processType === 'raw')
-            {
-                finishCallback = function() { successCallback(res); };
-            }
-
-            if (params.indicator)
-            {
-                params.indicator.finish(finishCallback);
-            }
-            else
-            {
-                finishCallback();
-            }
+            return response;
         };
 
-    this.setParam = function(name, value)
-    {
-        if (params[name] !== undefined)
-        {
-            params[name] = value;
-        }
-    };
 
-    this.getParam = function(name)
+    return function(endpoint, request, callback, params)
     {
-        return (params[name] !== undefined)
-            ? params[name]
-            : null;
-    };
+        params = params || {};
+        messageHandler = params.messageHandler || Agit.Service.get("messageHandler");
 
-    this.doCall = function()
-    {
+        if (endpoint instanceof Agit.Endpoint)
+            endpoint = endpoint.getName();
+
         var
-            request = 'request=' +
-                JSON.stringify(requestObject)
-                    .replace(/\+/g, '%2b')
-                    .replace(/&/g, '%26') +
-                "&locale=" + Agit.locale,
+            indicator = params.indicator || Agit.Service.get("indicator"),
+
+            callbackWrapper = function(response)
+            {
+                indicator.finish(function() {
+                    response = processResponse(response);
+                    showMessages(response.messageList);
+                    response.payload = normalizePayload(response.payload, response.entityList);
+                    callback(params.fullResponse ? response : response.payload);
+                });
+            },
 
             ajaxOpts = {
-                type         : 'POST',
-                url          : endpoint.getCallUrl(),
-                data         : request,
-                success      : combinedCallback,
-                error        : combinedCallback,
-                dataType     : params.dataType,
+                type         : "POST",
+                url          : Agit.sprintf('%s/%s', Agit.apiBaseUrl, endpoint),
+                data         : "request=" + JSON.stringify(request).replace(/\+/g, '%2b').replace(/&/g, '%26'),
+                success      : callbackWrapper,
+                error        : callbackWrapper,
+                dataType     : params.dataType || "json",
                 cache        : false
             };
 
-        if (params.dataType === 'jsonp')
+        if (ajaxOpts.dataType === "jsonp")
         {
-            ajaxOpts.type = 'GET';
-            ajaxOpts.url += '.jsonp';
+            ajaxOpts.type = "GET";
+            ajaxOpts.url += ".jsonp";
         }
 
         if (Agit.csrfToken)
         {
-            ajaxOpts.headers = { 'X-Token' : Agit.csrfToken };
+            ajaxOpts.headers = { "X-Token" : Agit.csrfToken };
         }
+
+        indicator && indicator.start();
 
         $.ajax(ajaxOpts);
-
-        if (params.indicator)
-        {
-            params.indicator.start();
-        }
     };
-};
-
-// shortcut
-Agit.apiCall = function(endpoint, requestObject, successCallback, params)
-{
-    return new Agit.ApiCall(endpoint, requestObject, successCallback, params).doCall();
-};
+})();
