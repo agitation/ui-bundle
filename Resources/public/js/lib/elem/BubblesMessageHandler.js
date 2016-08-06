97721fdd35c42f0926325421b35d8ba41f3a5585
Alexander Günsche <mail@lxg.de>
2016-08-06 18:12:01 +0200

    bugfix and refactoring in bubble message handler

--- Resources/public/js/lib/elem/BubblesMessageHandler.js
+++ Resources/public/js/lib/elem/BubblesMessageHandler.js
@@ -2,59 +2,59 @@ ag.ns("ag.ui.elem");
 
 (function(){
     var
-        msgH = function() {},
-
+        $container,
         $bubbles = {},
-
         removeBubble = function($msgBubble)
         {
             $msgBubble && $msgBubble.fadeOut(1500, $msgBubble.remove);
+        },
+
+        msgH = function()
+        {
+            if (!$container) $container = $("<div class='message-bubbles'>").appendTo($("body"));
         };
 
-    msgH.prototype = Object.create(ag.common.MessageHandler.prototype);
-    msgH.constructor = msgH;
-    ag.ui.elem.BubblesMessageHandler = msgH;
+msgH.prototype = Object.create(ag.common.MessageHandler.prototype);
 
-    msgH.prototype.clear = function(category)
-    {
-        Object.keys($bubbles).forEach(function(cat){
-            if (category === undefined || cat === category)
-            {
-                $bubbles[cat].forEach(function($msgBubble){
-                    removeBubble($msgBubble);
-                });
-            }
-        });
-    };
-
-    msgH.prototype.showMessage = function(message)
-    {
-        var
-            $msgBubble = ag.ui.tool.tpl("agitui-msg-bubbles", ".message-bubble"),
-            $container = $("<div class='message-bubbles'>").appendTo($("body")),
-            removeThisBubble = function(){ removeBubble($msgBubble); };
-
-        window.setTimeout(function(){
-            $msgBubble
-                .addClass(message.getType())
-                .appendTo($container)
-                .find(".icon i." + message.getType()).addClass("on").end()
-                .find(".msg").text(message.getText()).end()
-                .fadeIn(400)
-                .click(removeThisBubble)
-                .animate({ opacity:0.9 }, 1000);
-
-            // start disappearing after 10 seconds
-            window.setTimeout(removeThisBubble, 10000);
-        }, 200);
-
-        if (!$bubbles[message.getCategory()])
+msgH.prototype.clear = function(category)
+{
+    Object.keys($bubbles).forEach(function(cat){
+        if (category === undefined || cat === category)
         {
-            $bubbles[message.getCategory()] = [];
+            $bubbles[cat].forEach(function($msgBubble){
+                removeBubble($msgBubble);
+            });
         }
+    });
+};
 
-        $bubbles[message.getCategory()].push($msgBubble);
-    };
+msgH.prototype.showMessage = function(message)
+{
+    var
+        $msgBubble = ag.ui.tool.tpl("agitui-msg-bubbles", ".message-bubble"),
+        removeThisBubble = function() { removeBubble($msgBubble); };
 
+    window.setTimeout(function(){
+        $msgBubble
+            .addClass(message.getType())
+            .appendTo($container)
+            .find(".icon i." + message.getType()).addClass("on").end()
+            .find(".msg").text(message.getText()).end()
+            .fadeIn(400)
+            .click(removeThisBubble)
+            .animate({ opacity:0.9 }, 1000);
 
+        // start disappearing after 10 seconds
+        window.setTimeout(removeThisBubble, 10000);
+    }, 200);
+
+    if (!$bubbles[message.getCategory()])
+    {
+        $bubbles[message.getCategory()] = [];
+    }
+
+    $bubbles[message.getCategory()].push($msgBubble);
+};
+
+ag.ui.elem.BubblesMessageHandler = msgH;
 })();
